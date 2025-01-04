b4099b37ada2aa713b0f98002cea63f24ef1d31f
Aastha <aastha88soni@gmail.com>
2025-01-04 21:29:06 +0530

    initial commit

---
+++ portfolio/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <section className='pb-24 pt-40'>
+      <div className='container max-w-3xl'>
+        <h1>Portfolio</h1>
+      </div>
+    </section>
+  )
+}
