1739a3762b7980c813a0d9ba607aadbe92d9f217
Aastha <aastha88soni@gmail.com>
2025-01-05 20:42:02 +0530

    add dark theme

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Providers from '@/components/providers'
 import "./globals.css";
 
 const geistSans = Geist({
@@ -18,17 +19,19 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang='en' suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={inter.className}
       >
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
-  );
+  )
 }
